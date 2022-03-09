5ffc2fa080f0d14b1d3a1c18e1a254b045ce5474
minyeon9 <79310601+minyeon9@users.noreply.github.com>
2022-03-09 20:29:47 +0900

    0309_파일 업로드 기능 수정

--- 4Earth/src/main/webapp/resources/js/ui.js
+++ 4Earth/src/main/webapp/resources/js/ui.js
@@ -388,25 +388,21 @@ $(() => {
 	---------------------------------- */
     function handleFileSelect(event) {
         var input = this;
-        console.log(input.files)
+        
         if (input.files && input.files.length) {
             var reader = new FileReader();
             this.enabled = false;
             reader.onload = (function (e) {
+            
             console.log(e)
                 $(".upload-img").html(['<img src="', e.target.result, '" title="', escape(e.name), '"/>'].join(''))
             });
+            
             reader.readAsDataURL(input.files[0]);
         }
     }
     
     $('#inputFileOrigin').change(handleFileSelect);
-    $('.file-edit-icon').on('click', '.btn-delete-img', function () {
-        $(".upload-img").empty()
-        $("#inputFileOrigin").val("");
-    });
-
-
 
 });
 
@@ -432,7 +428,7 @@ document.addEventListener('DOMContentLoaded', function(){
         return (['image/jpeg','image/jpg', 'image/gif', 'image/png'].indexOf(img.type) > -1);
     }
 
-    var validateName = function(fname){
+    /* var validateName = function(fname){
         let extensions = ['jpeg','jpg', 'gif', 'png'];
         let fparts = fname.split('.');
         let fext = '';
@@ -452,7 +448,7 @@ document.addEventListener('DOMContentLoaded', function(){
         }
     
         return validated;
-    }
+    } */
 
     document.getElementById('inputFileOrigin').addEventListener('change', function(e){
         let elem = e.target;
@@ -460,14 +456,10 @@ document.addEventListener('DOMContentLoaded', function(){
         if( validateType(elem.files[0]) ) {
             let preview = document.querySelector('.upload-img img');
             preview.src = URL.createObjectURL(elem.files[0]);
-            document.querySelector('.btn-delete-img').style.display = 'block';
             preview.onload = function() {
                 URL.revokeObjectURL(preview.src);
             }
 
-            /* ----------------------------------
-                input upload file
-            ---------------------------------- */
             let inputFile = $('#inputFileOrigin');
             let inputName = $('.input-file');
 
@@ -478,17 +470,18 @@ document.addEventListener('DOMContentLoaded', function(){
             });
         } else {
             let inputName = $('.input-file');
-            alert("jpeg, jpg, gif, png 형식의 파일만 업로드 가능합니다.\n사진을 다시 업로드해주세요.");
+            alert("gif, png, jpg 형식의 파일만 업로드 가능합니다.\n사진을 다시 업로드해주세요.");
             $('#inputFileOrigin').val("");
         }
     });
-
+	
     document.querySelector('.btn-delete-img').addEventListener('click', function(e){
         let preview = document.querySelector('.upload-img img');
         let inputName = $('.input-file');
         preview.src = '';
-        // document.querySelector('.btn-delete-img').style.display = 'none';
         inputName.val("파일을 선택해주세요.");
+        $(".upload-img").empty()
+        $("#inputFileOrigin").val("");
     });
 });
 
